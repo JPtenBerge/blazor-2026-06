94621bb40df93216bb35e5ae8bdab83aa62468a1
JP <jp@jp.nl>
2026-06-08 11:26:16 +0200

    Projecten verkennen en databinding laten zien

---
+++ DemoSolution/DemoProject/Components/Pages/Home.razor.cs
@@ -0,0 +1,35 @@
+﻿using DemoProject.Entities;
+
+namespace DemoProject.Components.Pages;
+
+public partial class Home
+{
+    public List<Person>? People { get; set; } = new()
+    {
+        new()
+        {
+            Age = 63,
+            Name = "Bob",
+            PhotoUrl = "https://example.com/photos/bob.jpg"
+        },
+        new()
+        {
+            Age = 30,
+            Name = "Alice",
+            PhotoUrl = "https://example.com/photos/alice.jpg"
+        },
+        new()
+        {
+            Age = 23,
+            Name = "Eve",
+            PhotoUrl = "https://example.com/photos/eve.jpg"
+        }
+    };
+
+    public string Name { get; set; } = "Luuk";
+
+    string DoeIets()
+    {
+        return $"Hallo, {Name}!";
+    }
+}
---
+++ DemoSolution/DemoProject/Entities/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoProject.Entities;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string PhotoUrl { get; set; }
+}
