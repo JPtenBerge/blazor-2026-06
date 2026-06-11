115e7801a60348a2656a3b25baea17268300da50
JP <jp@jp.nl>
2026-06-11 15:18:54 +0200

    Authenticatie werkend

--- DemoSolution/BlazorApp1/BlazorApp1.Client/Program.cs
+++ DemoSolution/BlazorApp1/BlazorApp1.Client/Program.cs
@@ -1,8 +1,13 @@
 using BlazorApp1.Client.Repositories;
 using DemoProject.Shared.Repositories;
+using Duende.Bff.Blazor.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+    .AddBffBlazorClient() // Provides auth state provider that polls the /bff/user endpoint
+    .AddCascadingAuthenticationState();
 
 //builder.Services.AddSingleton<IPersonRepository, PersonRepository>();
 builder.Services.AddSingleton<IPersonRepository, PersonHttpRepository>();
--- DemoSolution/DemoProject/Program.cs
+++ DemoSolution/DemoProject/Program.cs
@@ -1,13 +1,15 @@
+using DemoProject.Apis;
 using DemoProject.Components;
 using DemoProject.DataAccess;
-using DemoProject.Shared.Entities;
 using DemoProject.Repositories;
+using DemoProject.Shared.Entities;
+using DemoProject.Shared.Repositories;
+using DemoProject.Shared.Validators;
+using Duende.Bff;
+using Duende.Bff.Blazor;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
-using DemoProject.Shared.Repositories;
-using DemoProject.Shared.Validators;
-using DemoProject.Apis;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,6 +36,53 @@ builder.Services.AddDbContextFactory<DemoContext>(options =>
 //    });
 //});
 
+builder.Services.AddBff()
+    .AddServerSideSessions() // Add in-memory implementation of server side sessions
+    .AddBlazorServer();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "cookie";
+    options.DefaultChallengeScheme = "oidc";
+    options.DefaultSignOutScheme = "oidc";
+})
+    .AddCookie("cookie", options =>
+    {
+        options.Cookie.Name = "__Host-blazor";
+
+        // Because we use an identity server that's configured on a different site
+        // (duendesoftware.com vs localhost), we need to configure the SameSite property to Lax. 
+        // Setting it to Strict would cause the authentication cookie not to be sent after loggin in.
+        // The user would have to refresh the page to get the cookie.
+        // Recommendation: Set it to 'strict' if your IDP is on the same site as your BFF.
+        options.Cookie.SameSite = SameSiteMode.Lax;
+    })
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.ClientId = "blazorfrontend";
+        options.ClientSecret = "304F15BB-6C25-4BCC-A741-0BED7C0DCB10";
+        options.ResponseType = "code";
+        options.ResponseMode = "query";
+
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.SaveTokens = true;
+        options.MapInboundClaims = false;
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        //options.Scope.Add("api");
+        options.Scope.Add("offline_access");
+
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+    });
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -56,7 +105,17 @@ if (!app.Environment.IsDevelopment())
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+app.UseRouting();
+app.UseAuthentication();
+
+// Add the BFF middleware which performs anti forgery protection
+app.UseBff();
+app.UseAuthorization();
+
+
 app.UseAntiforgery();
+app.MapBffManagementEndpoints();
+
 
 //app.UseCors("blazorfrontend");
 
