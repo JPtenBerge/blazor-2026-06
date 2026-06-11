701299d5022273ae241abd42debeb249dbb4cebd
JP <jp@jp.nl>
2026-06-11 15:17:02 +0200

    IdentityProvider-project

---
+++ DemoSolution/IdentityProvidert/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Duende.IdentityServer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
+
+namespace IdentityProvidert.Pages
+{
+    [AllowAnonymous]
+    public class Index : PageModel
+    {
+        public Index(IdentityServerLicense? license = null) => License = license;
+
+        public string Version => typeof(Duende.IdentityServer.Hosting.IdentityServerMiddleware).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion.Split('+').First()
+                ?? "unavailable";
+
+        public IdentityServerLicense? License { get; }
+    }
+}
